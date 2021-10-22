44d04885760bd328083c7d416dcb6178524c9b4d
Aaron <afernandezr@cifpfbmoll.eu>
2021-10-22 19:43:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int contador =0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View bContador = findViewById(R.id.button_count);
+        View bToast = findViewById(R.id.button_toast);
+
+        TextView textView = (TextView) findViewById(R.id.show_count);
+        textView.setText(String.valueOf(contador));
+        bContador.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                contador++;
+                textView.setText(String.valueOf(contador));
+            }
+        });
+        bToast.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), String.valueOf(contador), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
