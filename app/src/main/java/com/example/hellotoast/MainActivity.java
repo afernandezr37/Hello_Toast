ba529ff06c6d7f6c3d3f729866073ab82f1369d0
Aaron <afernandezr@cifpfbmoll.eu>
2021-10-22 20:33:10 +0200

    Exercici 12b

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -12,13 +12,25 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int contador =0;
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState((savedInstanceState));
+        savedInstanceState.putInt("contador", contador);
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View bContador = findViewById(R.id.button_count);
         View bToast = findViewById(R.id.button_toast);
-
+        if (savedInstanceState != null) {
+            contador = savedInstanceState.getInt("contador", 0);
+        }
         TextView textView = (TextView) findViewById(R.id.show_count);
         textView.setText(String.valueOf(contador));
         bContador.setOnClickListener(new View.OnClickListener() {
@@ -33,4 +45,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
